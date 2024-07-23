3d993535399d1a7b5f93922f4af3ff731386fa30
Yuxin Zhou <kathrinezhou@gmail.com>
2024-07-22 21:35:49 -0400

    Pacemaker

---
+++ DCM/src/__init__.py
@@ -0,0 +1,193 @@
+import json
+from flask import Flask, render_template, request, redirect, url_for, session
+import sqlite3
+import serial
+import database as db
+import utils
+#import serial_utils
+import dummy_write_file as serial_utils
+
+parameter_ranges = {
+    'lower_rate_limit': {"range": [50, 90], "type": int},
+    'atrial_amplitude': {"range": [65, 75], "type": int},
+    'atrial_pulse_width': {"range": [1, 30], "type": int},  # ms
+    'atrial_sensitivity': {"range": [25, 100], "type": float},
+    'arp': {"range": [150, 500], "type": int},
+    'ventricle_amplitude': {"range": [65, 75], "type": int},
+    'ventricle_pulse_width': {"range": [1, 30], "type": int},
+    'ventricle_sensitivity': {"range": [25, 100], "type": float},
+    'vrp': {"range": [150, 500], "type": int},
+    'max_sens_rate': {"range": [50, 175], "type": int},
+    'activity_threshold': {"range": [0, 200], "type": int},
+    'reaction_time': {"range": [0, 200], "type": int},
+    'response_factor': {"range": [1, 20], "type": int},
+    'recovery_time': {"range": [0, 200], "type": int},
+    'fixed_av_delay': {'range': [70, 300], "type": int}
+}
+
+# Set up the server
+app = Flask(__name__)
+app.secret_key = b'!@#$RFBNKOI*&^%RESXCVBNKLOI*&^%ESXCVBNK<BVCDERTYHJ'
+app.config['DATABASE'] = '.\\data\\db.sql'
+db.init_db(app)
+
+# Set up the serial output, port isn't set
+# so that the user can input it later
+ser = serial.Serial()
+ser.baudrate = 115200
+
+
+# Redirect to the login page
+@app.route('/')
+def landing_page():
+    return redirect(url_for('login'))
+
+
+# Add a new user and add them to the database
+# If database is full or the user already exists then the registration will fail
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if request.method == "GET":
+        return render_template('signup.html', signup_failed=False)
+
+    if request.method == "POST":
+        if db.get_num_users() >= 10:
+            return render_template('signup.html', signup_failed=True, error_msg="too many users registered")
+        if request.form['username'] and request.form['password']:
+            try:
+                db.create_user((request.form['username'], request.form['password']))
+                return redirect(url_for('login'))
+            except sqlite3.IntegrityError as e:
+                print(e)
+                return render_template('signup.html', signup_failed=True,
+                                       error_msg="unsuccessful registration, try again")
+        return render_template('signup.html', signup_failed=True, error_msg="no username or password entered")
+
+
+# If user navigates to /login (method == get), serve the login
+# page. If user is already logged in, redirect to /dcm
+
+# If user submits their login information (method == post),
+# check if the user's information is valid. If valid, send to the dcm page.
+# If not, send them back to the login page with a notification
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        if "user" not in session:
+            return render_template('index.html',
+                                   login_failed=
+                                   False if 'login_failed' not in request.args
+                                   else request.args['login_failed']
+                                   )
+        else:
+            return redirect(url_for("dcm_view"))
+
+    if request.method == 'POST':
+
+        # check if login input is valid
+        user = utils.check_credentials(request.form['username'], request.form['password'])
+
+        if user:
+            session["device_id"] = 0
+            session["user"] = user
+            return redirect(url_for("dcm_view"))
+        else:
+            return redirect(url_for('login', login_failed=True))
+
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.pop("user")
+    session.pop("device_id")
+    return redirect("/")
+
+
+@app.route('/connection_update', methods=['POST'])
+def connect():
+    session["serial_port"] = request.form["serial_port"]
+    ser.port = session["serial_port"]
+    try:
+        session['device_id'] = serial_utils.get_device_id(ser)
+    except serial.serialutil.SerialException:
+        session['device_id'] = None
+        session["serial_port"] = "Failed To Connect"
+    return redirect("/dcm")
+
+
+# Show the user the page with dcm settings. Clicking a different mode
+# shows a different submission form with the valid parameters (see dcm.html)
+@app.route('/dcm', methods=['GET'])
+def dcm_view():
+    if "user" not in session:
+        return redirect(url_for("landing_page"))
+    else:
+        device_id = None if "device_id" not in session else session["device_id"]
+        user_stored_params = db.get_user_params(session["user"], device_id)
+        submission_success = None if 'submission_success' not in request.args else request.args['submission_success']
+        invalid_parameters = None if 'invalid_parameters' not in request.args else request.args['invalid_parameters']
+        serial_port = "None" if "serial_port" not in session else session["serial_port"]
+        return render_template('dcm.html',
+                               device_id=device_id,
+                               user=session["user"],
+                               stored_params=user_stored_params,
+                               submission_success=submission_success,
+                               invalid_parameters=invalid_parameters,
+                               serial_port=serial_port,
+                               parameter_ranges=parameter_ranges
+                               )
+
+
+# After submitting a form on the dcm page, the results are posted here.
+# The parameters are checked if they are within predefined valid ranges, and
+# if they are not, send the user a message with which parameters are out of the range.
+
+# TODO: Add serial communication to send the parameters sent to this method to simulink using ser.write()
+# TODO: Parameters are accessed with request.form[parameter]
+@app.route('/submit-params/<mode>', methods=['POST'])
+def submit_params(mode):
+    invalid_parameters = utils.check_invalid_parameters(request.form, mode, parameter_ranges)
+    print("invalid_parameters")
+    print(invalid_parameters)
+    if invalid_parameters or not session['device_id']:
+        return redirect(url_for("dcm_view",
+                                submission_success=False,
+                                invalid_parameters=invalid_parameters)
+                        )
+    (parameters, parameter_dict) = utils.build_parameters(mode, request.form)
+    db.create_parameters(parameters, session['user'], session['device_id'])
+
+    try:
+        if not serial_utils.set_device_params(ser, parameter_dict):
+            raise serial.serialutil.SerialException
+    except serial.serialutil.SerialException:
+        session['device_id'] = None
+        return redirect(url_for("dcm_view",
+                                submission_success=False,
+                                invalid_parameters=invalid_parameters)
+                        )
+    # TODO: This is where to put the serial communication
+    print(request.form)
+
+    return redirect(url_for("dcm_view",
+                            submission_success=True,
+                            invalid_parameters=invalid_parameters)
+                    )
+
+
+@app.route('/get_egram_data', methods=['GET'])
+def get_egram_data():
+    egram_data = {'atrium': -10, 'ventricle': -10}
+    try:
+        egram_data = serial_utils.receive_egram_transmission(ser)
+        serial_success = 200
+    except serial.serialutil.SerialException:
+        serial_success = 500
+    return app.response_class(
+        response=json.dumps(
+            {'status': serial_success, 'atrium': egram_data['atrium'], 'ventricle': egram_data['ventricle']}),
+        mimetype='application/json'
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ DCM/src/database.py
@@ -0,0 +1,137 @@
+import sqlite3
+from flask import current_app, g
+
+
+def get_num_users():
+    cursor = get_db().execute('SELECT COUNT(*) FROM users')
+    result = cursor.fetchone()
+    cursor.close()
+    print('Number of users: %s' % result[0])
+    return result[0]
+
+
+def get_user_params(user, device_connection):
+    try:
+        cursor = get_db().execute('SELECT * FROM parameters WHERE username=?'
+                                  'AND device_id=?', (user, device_connection))
+        result = cursor.fetchone()
+        cursor.close()
+        return result
+    except sqlite3.OperationalError as e:
+        return None
+
+
+def query_user(user):
+    cursor = get_db().execute('SELECT * FROM users WHERE username=?', (user,))
+    result = cursor.fetchone()
+    cursor.close()
+    return result
+
+
+def create_parameters(parameter_data, user, device_id):
+    db = get_db()
+    cursor = db.cursor()
+
+    sql_query = ''' 
+        INSERT INTO parameters 
+        (
+            username,
+            device_id,
+            mode,
+            lower_rate_limit,
+            upper_rate_limit,
+            atrial_amplitude,
+            atrial_pulse_width,
+            atrial_sensitivity,
+            arp,
+            pvarp,
+            ventricle_amplitude,
+            ventricle_pulse_width,
+            ventricle_sensitivity,
+            vrp,
+            pvarp_ext,
+            hysteresis,
+            rate_smoothing,
+            fixed_av_delay,
+            dynamic_av_delay,
+            atr_duration,
+            atr_fall_mode,
+            atr_fall_time,
+            max_sens_rate,
+            activity_threshold,
+            reaction_time,
+            response_time,
+            recovery_time
+        )
+        VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
+        ON CONFLICT (username, device_id) DO UPDATE SET 
+            mode=?,
+            lower_rate_limit=?,
+            upper_rate_limit=?,
+            atrial_amplitude=?,
+            atrial_pulse_width=?,
+            atrial_sensitivity=?,
+            arp=?,
+            pvarp=?,
+            ventricle_amplitude=?,
+            ventricle_pulse_width=?,
+            ventricle_sensitivity=?,
+            vrp=?,
+            pvarp_ext=?,
+            hysteresis=?,
+            rate_smoothing=?,
+            fixed_av_delay=?,
+            dynamic_av_delay=?,
+            atr_duration=?,
+            atr_fall_mode=?,
+            atr_fall_time=?,
+            max_sens_rate=?,
+            activity_threshold=?,
+            reaction_time=?,
+            response_time=?,
+            recovery_time=?
+    '''
+
+    parameters = (user, device_id) + parameter_data + parameter_data
+    cursor.execute(sql_query, parameters)
+    db.commit()
+
+
+def create_user(user_data):
+    db = get_db()
+    sql_query = 'INSERT INTO users(' \
+                'username,password' \
+                ')VALUES(?,?)'
+    cursor = db.cursor()
+    cursor.execute(sql_query, user_data)
+    db.commit()
+
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+def init_db(app):
+    with app.app_context():
+        db = get_db()
+
+        with app.open_resource('./parameters_schema.sql') as schema:
+            try:
+                db.executescript(schema.read().decode('utf8'))
+            except sqlite3.OperationalError as e:
+                print(e)
+        db.commit()
+
+        with app.open_resource('./users_schema.sql') as schema:
+            try:
+                db.executescript(schema.read().decode('utf8'))
+            except sqlite3.OperationalError as e:
+                print(e)
+        db.commit()
